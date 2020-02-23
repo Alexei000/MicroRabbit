d4ad17c3662f6a502032f25564c2877baab24518
Alexandru Dragan <alexandru.dragan@gmail.com>
2020-02-23 10:52:40 +0200

    implemented interfaces and classes for Domain.Core

---
+++ MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<TEvent, THandler>() 
+            where TEvent : Event 
+            where THandler : IEventHandler<TEvent>;
+    }
+}
